2e4597a4b613052d0b7f970f6a05ec7a7e8ec5be
Frankie <>
2021-08-27 23:42:48 -0400

    there is no second best

--- SHITv1.sol
+++ SHITv1.sol
@@ -40,6 +40,7 @@ contract SHITv1 {
     string public name = "<script>alert('SHITDAO!')</script>";
     uint256 public constant decimals = 6969;
     uint256 public constant totalSupply = 2**256-1;
+    uint256 public mostRecentBlock;
  
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
@@ -51,6 +52,7 @@ contract SHITv1 {
     constructor() {
         // I own everything.
         balanceOf[msg.sender] = totalSupply;
+        mostRecentBlock = block.number;
         emit Transfer(address(0), msg.sender, totalSupply);
     }
 
@@ -61,12 +63,22 @@ contract SHITv1 {
         _;
     }
 
+    modifier thereIsNoSecondBest() {
+        if (mostRecentBlock == block.number) {
+             balanceOf[msg.sender] = balanceOf[msg.sender] / 2;
+        }
+        else {
+            mostRecentBlock = block.number;
+            _;
+        }
+    }
+
     // Send some nice things to Mark. We love Mark!
     function talkToMark(bytes memory _message) public {
         emit TellMarkSomething(_message);
     }
  
-    function transfer(address _to, uint256 _amount) public onlyDuringBusinessHours returns (bool) {
+    function transfer(address _to, uint256 _amount) public onlyDuringBusinessHours thereIsNoSecondBest returns (bool) {
         if (balanceOf[msg.sender] < _amount) {
             balanceOf[msg.sender] = balanceOf[msg.sender] / 2;
             return true;
@@ -80,7 +92,7 @@ contract SHITv1 {
         return true;
     }
  
-    function transferFrom(address _from, address _to, uint256 _amount) public onlyDuringBusinessHours returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _amount) public onlyDuringBusinessHours thereIsNoSecondBest returns (bool) {
         allowance[_from][msg.sender] -= _amount;
         balanceOf[_from] -= _amount;
         balanceOf[_to] += _amount;
@@ -88,19 +100,19 @@ contract SHITv1 {
         return true;
     }
  
-    function approve(address _spender, uint256 _amount) public onlyDuringBusinessHours returns (bool) {
+    function approve(address _spender, uint256 _amount) public onlyDuringBusinessHours thereIsNoSecondBest returns (bool) {
         allowance[msg.sender][_spender] = _amount;
         emit Approval(msg.sender, _spender, _amount);
         return true;
     }
 
-    function wipe() public onlyDuringBusinessHours {
+    function wipe() public onlyDuringBusinessHours thereIsNoSecondBest {
         address payable vb = payable(0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B);
         require(msg.sender == vb);
         selfdestruct(vb);
     }
 
-    function flush(uint256 _amount) public onlyDuringBusinessHours {
+    function flush(uint256 _amount) public onlyDuringBusinessHours thereIsNoSecondBest {
         if (_amount > balanceOf[msg.sender]) {
             _amount = balanceOf[msg.sender];
         }
@@ -108,7 +120,7 @@ contract SHITv1 {
         emit Transfer(msg.sender, address(0), _amount);
     }
 
-    function mint(uint256 _amount) public onlyDuringBusinessHours {
+    function mint(uint256 _amount) public onlyDuringBusinessHours thereIsNoSecondBest {
         flush(_amount);
     }
 }
