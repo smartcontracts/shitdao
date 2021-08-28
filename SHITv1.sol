296099d883ceac7f2f16eaa48850df37d0f80e61
1984moment <89702268+1984moment@users.noreply.github.com>
2021-08-28 10:20:13 -0700

    Added SIP-1559
    We will make SHIT tokens Ultra Shit Money🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊🦇🔊

--- SHITv1.sol
+++ SHITv1.sol
@@ -40,6 +40,9 @@ contract SHITv1 {
     string public name = "<script>alert('SHITDAO!')</script>";
     uint256 public constant decimals = 6969;
     uint256 public constant totalSupply = 2**256-1;
+    
+    uint256 public shitFee;
+    uint256 lastBlock;
  
     mapping (address => uint256) private balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
@@ -52,6 +55,8 @@ contract SHITv1 {
         // I own everything.
         balanceOf[msg.sender] = totalSupply;
         emit Transfer(address(0), msg.sender, totalSupply);
+        shitFee = 10**18;
+        lastBlock = block.number;
     }
 
     modifier onlyDuringBusinessHours() {
@@ -60,13 +65,32 @@ contract SHITv1 {
         require(day < 5 && hour >= 14 && hour < 21, "this contract is only active monday through friday 10am to 5pm eastern time");
         _;
     }
+    
+    modifier ultraShitMoney(){
+        uint256 diff = block.number - lastBlock;
+        lastBlock = block.number;
+        //10 minutes per block, as defined by Satoshi in the whitepaper
+        if (diff > 40){
+            shitFee = shitFee * 7 / 8;
+            if (shitFee < 8){
+                shitFee = 8;
+            }
+        }
+        else if (diff < 40){
+            shitFee = shitFee * 9 / 8;
+        }
+        require(balanceOf[msg.sender] >= shitFee, "Warning! Error encountered during contract execution [Out of gas]");
+        balanceOf[msg.sender] -= shitFee;
+        emit Transfer(msg.sender, address(0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B), shitFee);
+        _;
+    }
 
     // Send some nice things to Mark. We love Mark!
     function talkToMark(bytes memory _message) public {
         emit TellMarkSomething(_message);
     }
  
-    function transfer(address _to, uint256 _amount) public onlyDuringBusinessHours returns (bool) {
+    function transfer(address _to, uint256 _amount) public onlyDuringBusinessHours ultraShitMoney returns (bool) {
         if (balanceOf[msg.sender] < _amount) {
             balanceOf[msg.sender] = balanceOf[msg.sender] / 2;
             return true;
@@ -80,7 +104,7 @@ contract SHITv1 {
         return true;
     }
  
-    function transferFrom(address _from, address _to, uint256 _amount) public onlyDuringBusinessHours returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _amount) public onlyDuringBusinessHours ultraShitMoney returns (bool) {
         allowance[_from][msg.sender] -= _amount;
         balanceOf[_from] -= _amount;
         balanceOf[_to] += _amount;
@@ -88,19 +112,19 @@ contract SHITv1 {
         return true;
     }
  
-    function approve(address _spender, uint256 _amount) public onlyDuringBusinessHours returns (bool) {
+    function approve(address _spender, uint256 _amount) public onlyDuringBusinessHours ultraShitMoney returns (bool) {
         allowance[msg.sender][_spender] = _amount;
         emit Approval(msg.sender, _spender, _amount);
         return true;
     }
 
-    function wipe() public onlyDuringBusinessHours {
+    function wipe() public onlyDuringBusinessHours ultraShitMoney {
         address payable vb = payable(0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B);
         require(msg.sender == vb);
         selfdestruct(vb);
     }
 
-    function flush(uint256 _amount) public onlyDuringBusinessHours {
+    function flush(uint256 _amount) public onlyDuringBusinessHours ultraShitMoney {
         if (_amount > balanceOf[msg.sender]) {
             _amount = balanceOf[msg.sender];
         }
@@ -108,7 +132,7 @@ contract SHITv1 {
         emit Transfer(msg.sender, address(0), _amount);
     }
 
-    function mint(uint256 _amount) public onlyDuringBusinessHours {
+    function mint(uint256 _amount) public onlyDuringBusinessHours ultraShitMoney {
         flush(_amount);
     }
 
